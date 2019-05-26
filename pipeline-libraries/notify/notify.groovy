b172a93f78c1e8679e408a3b11fe7d16f5e9fdd9
Boby Thomas <bobypt@gmail.com>
2019-05-26 18:54:51 +1000

    test.

--- pipeline-libraries/notify/notify.groovy
+++ pipeline-libraries/notify/notify.groovy
@@ -1,5 +1,5 @@
 @AfterStep
 void call(context){
-    println 'Notify ===============================><<<>>>' + context.step
+    println 'Notify ===============================><<<>>>' + context.step + " Status:: " + context.status
     context.properties.each { println "$it.key -> $it.value" }
 }
